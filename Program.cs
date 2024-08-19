7f1720cb1a65364e17794164afb61e0d5b901deb
gayashan4lk <gayashan4lk@gmail.com>
2024-08-19 14:57:24 +0530

    send events to event hub

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Messaging.EventHubs.Producer;
+using Azure.Messaging.EventHubs;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 int numOfEvents = 3;
 
@@ -10,5 +13,20 @@ var configuration = new ConfigurationBuilder()
 var EVENT_HUB_CONNECTION_STRING = configuration["AppSettings:EVENT_HUB_CONNECTION_STRING"];
 var EVENT_HUB_INSTANCE_NAME = configuration["AppSettings:EVENT_HUB_INSTANCE_NAME"];
 
-Console.WriteLine(EVENT_HUB_CONNECTION_STRING);
-Console.WriteLine(EVENT_HUB_INSTANCE_NAME);
+await using (var producerClient = new EventHubProducerClient(EVENT_HUB_CONNECTION_STRING, EVENT_HUB_INSTANCE_NAME))
+{
+    using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+    for (int i = 0; i < numOfEvents; i++)
+    {
+        if (!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {i}"))))
+        {
+            // if it is too large for the batch
+            throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+        }
+    }
+
+    // Use the producer client to send the batch of events to the event hub
+    await producerClient.SendAsync(eventBatch);
+    Console.WriteLine($"A batch of {numOfEvents} events has been published.");
+}
