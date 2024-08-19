089ceb6e27a742197585ba5ff1cbdcee23f1522b
gayashan4lk <gayashan4lk@gmail.com>
2024-08-19 14:52:27 +0530

    add config secrets by setting.json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+int numOfEvents = 3;
+
+var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("settings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+var EVENT_HUB_CONNECTION_STRING = configuration["AppSettings:EVENT_HUB_CONNECTION_STRING"];
+var EVENT_HUB_INSTANCE_NAME = configuration["AppSettings:EVENT_HUB_INSTANCE_NAME"];
+
+Console.WriteLine(EVENT_HUB_CONNECTION_STRING);
+Console.WriteLine(EVENT_HUB_INSTANCE_NAME);
